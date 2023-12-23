a343b62838c280a9d13cfdb9a97aea52e30a0d71
enya <anna361362@gmail.com>
2023-12-23 13:39:43 +0300

    added 1 test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,21 @@ public class Main{
         List listEven = new List(3);    // 3 even elevators
         List listOdd = new List(3);    // 3 odd elevators
         List listStaff = new List(1);    // maybe there will be more in the future
-        // creation of elevators: 3, 3, 1
+        // creation of elevators: 3 even, 3 odd, 1 staff
         for (int i = 0; i < elevators.length; ++i) {
             if (i < 3) elevators[i] = new EvenElevator(Options.EVEN);
             else if (i < 6) elevators[i] = new OddElevator(Options.ODD);
             else elevators[i] = new StaffElevator(Options.STAFF);
         }
+        try {
+            call(elevators, listEven, listOdd, listStaff);
+        }
+        catch (ElevatorsBusy a) {
+            System.out.println(a.getMessage());
+        }
+
+        // set 1 even elevator free & try again
+        elevators[0].setStatus(Status.FREE);
         try {
             call(elevators, listEven, listOdd, listStaff);
         }
